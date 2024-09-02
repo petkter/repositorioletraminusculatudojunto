d9a951fe430c27957f2eb639adf864c17c106597
petkter <luispetteralvarado@gmail.com>
2024-09-02 19:26:27 -0300

    20,0

---
+++ Calculadora/App.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner mmgvo = new Scanner(System.in);
+        System.out.println("Bienvenido a la calculadora ");
+        int op = 0;
+        int numerouno;
+        int numerodos;
+        System.out.println("Elija una opcion de calculo ");
+        System.out.println("Digite el numero 1 para adicion ");
+        System.out.println("Digite el numero 2 para substracion ");
+        System.out.println("Opcion: ");
+        op = mmgvo.nextInt(); 
+        do {
+        switch (op) {
+            case 1: 
+                
+                break;
+            case 2:
+                break;
+            default:
+                break;
+        }
+            
+        } while (!=0);
+    }
+}
